3f23bdd25f691cbd99f6a1e222474162339536d8
Mikkel Milo <mikkelmilo@gmail.com>
2021-06-18 21:17:24 +0200

    macOS safe?

--- examples/typecheck_examples.sh
+++ examples/typecheck_examples.sh
@@ -35,17 +35,18 @@ function typecheck {
     echo "    extracting coq ..."
     mkdir -p target/coq
     cratename=$1
-  #  fname=$(echo $1| "hacspec-")
     prefix="hacspec-"
     outname=${cratename#"$prefix"}
-    cargo hacspec -o target/coq/${outname^}.v $1
+    # capitalize first letter - macOS safe version (bash <= 3.2)
+    outname=$(tr '[:lower:]' '[:upper:]' <<<"${outname:0:1}")${outname:1}
+    cargo hacspec -o target/coq/${outname}.v $1
   fi
 }
 
 cd $(dirname "$0")/../
-cargo clean
-cargo build
-cargo install --path language
+#cargo clean
+#cargo build
+#cargo install --path language
 typecheck hacspec-chacha20             ec      fst    json      coq  Hacspec.Chacha20.fst
 typecheck hacspec-chacha20poly1305     ec      fst    json      coq  Hacspec.Chacha20Poly1305.fst
 typecheck hacspec-poly1305             ec      fst    json      coq  Hacspec.Poly1305.fst
